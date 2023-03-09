2fe4c4863dba7a9faed0b741f32ea85263ecda9a
Radek Novotny <rano@ciklum.com>
2023-03-09 17:46:39 +0100

    Cleanup

--- src/PatientDatabase.Client/Pages/DataUpload.fs
+++ src/PatientDatabase.Client/Pages/DataUpload.fs
@@ -10,38 +10,15 @@ open PatientDatabase.Client.Server
 open PatientDatabase.Shared.API
 open System
 
-type private State = { Form: PatientForm; InputFile: File option }
+type private State = { InputFile: File option }
 
 type private Msg =
-    | FormChanged of PatientForm
-    | FormSubmitted
-    | FormSaved of unit
+    | FileSaved of unit
     | FileChosen of File
     | FileUploaded
 
 let private init () =
     {
-        Form = {
-            Place = "Nebbi"
-            Date = DateTime.Today.ToShortDateString()
-            Name = "John Doe"
-            Age = "13"
-            Sex = "Unknown"
-            Symptom1 = ""
-            Symptom2 = ""
-            Symptom3 = ""
-            Tests = "HIV"
-            Test1 = ""
-            Test2 = ""
-            Test3 = ""
-            Result1 = "negative"
-            Result2 = "negative"
-            Result3 = "negative"
-            Diagnosis1 = ""
-            Diagnosis2 = ""
-            Diagnosis3 = ""
-            Treatment = "ibuprofen"
-        }
         InputFile = None
     },
     Cmd.none
@@ -54,14 +31,12 @@ let upload (file: File) = async {
 
 let private update (msg: Msg) (model: State) : State * Cmd<Msg> =
     match msg with
-    | FormChanged patientForm -> { model with Form = patientForm }, Cmd.none
-    | FormSubmitted -> model, Cmd.OfAsync.perform service.SaveForm model.Form FormSaved
-    | FormSaved _ -> model, Cmd.none
+    | FileSaved _ -> model, Cmd.none
     | FileChosen file -> { model with InputFile = Some file }, Cmd.none
     | FileUploaded ->
          match model.InputFile with
          | None -> model, Cmd.none
-         | Some file -> model, Cmd.OfAsync.perform upload file FormSaved
+         | Some file -> model, Cmd.OfAsync.perform upload file FileSaved
 
 [<ReactComponent>]
 let IndexView () =
@@ -69,24 +44,32 @@ let IndexView () =
 
 
     Html.div [
-        Html.form [
-            prop.onSubmit (fun e ->
-                e.preventDefault ()
-                FileUploaded |> dispatch)
-            prop.className "flex flex-col items-center"
-            prop.children [
-                Daisy.formControl [
-                    Daisy.label [Daisy.labelText "Upload data from file"]
-                    Daisy.file [
-                        file.bordered
-                        prop.onChange (fun value -> value |> FileChosen |> dispatch)
+        prop.className "flex flex-col items-center"
+        prop.children [
+            Daisy.card [
+                prop.className "p-6 bg-base-200 shadow-xl m-2"
+                prop.children [
+                    Html.form [
+                        prop.onSubmit (fun e ->
+                            e.preventDefault ()
+                            FileUploaded |> dispatch)
+                        prop.className "flex flex-col items-center"
+                        prop.children [
+                            Daisy.formControl [
+                                Daisy.label [Daisy.labelText "Upload data from file"]
+                                Daisy.file [
+                                    file.bordered
+                                    prop.onChange (fun value -> value |> FileChosen |> dispatch)
+                                ]
+                            ]
+                            Daisy.button.button [
+                                prop.className "btn-block m-2"
+                                button.outline
+                                prop.text "Upload Data"
+                                prop.type'.submit
+                            ]
+                        ]
                     ]
-                ]
-                Daisy.button.button [
-                    prop.className "btn-wide shadow-2xl m-2"
-                    button.outline
-                    prop.text "Upload Data"
-                    prop.type'.submit
                 ]
             ]
         ]
